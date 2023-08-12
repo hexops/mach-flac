7ded2f244973b9c966c3210e6dc14fe447113fa2
Ali Chraghi <alichraghi@proton.me>
2023-08-12 09:54:46 +0330

    all: replace test with sysaudio example

--- build.zig
+++ build.zig
@@ -1,23 +1,28 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const sysaudio = @import("mach_sysaudio");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("mach-flac", .{ .source_file = .{ .path = "src/lib.zig" } });
-
+    const module = b.addModule("mach-flac", .{ .source_file = .{ .path = "src/lib.zig" } });
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
     const libflac_dep = b.dependency("flac", .{ .target = target, .optimize = optimize });
 
-    const main_test = b.addTest(.{
-        .root_source_file = .{ .path = "src/lib.zig" },
+    const example = b.addExecutable(.{
+        .name = "example-play",
+        .root_source_file = .{ .path = "examples/play.zig" },
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(main_test);
-    main_test.linkLibrary(libflac_dep.artifact("flac"));
-    main_test.main_pkg_path = .{ .path = "." };
+    example.addModule("flac", module);
+    example.addModule("sysaudio", sysaudio.module(b, optimize, target));
+    example.linkLibrary(libflac_dep.artifact("flac"));
+    sysaudio.link(b, example, .{});
+    b.installArtifact(example);
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&b.addRunArtifact(main_test).step);
+    const example_run_cmd = b.addRunArtifact(example);
+    example_run_cmd.step.dependOn(b.getInstallStep());
+
+    const example_run_step = b.step("run-example", "Run example");
+    example_run_step.dependOn(&example_run_cmd.step);
 }
